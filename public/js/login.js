5064a4d300bcb1d71ce99e60def62fe3955881ab
Dorian Maldonado <d1maldon@ucsd.edu>
2021-02-24 16:13:52 -0800

    Set up login button

--- public/js/login.js
+++ public/js/login.js
@@ -6,24 +6,15 @@ $(document).ready(function () {
 } );
 
 function initializePage() {
-    //code here
     setUpLogInButton();
 }
 
 function setUpLogInButton() {
     $('#log').click(() => {
-        console.log("Log in button clicked");
-        window.location.href = "localhost:3000/"; 
+        //use if viewing from local node server
+        window.location.href = "http://localhost:3000/"; 
+
+        //use if viewing from remote heroku server
+        // window.location.href = "herokulink.com/login"
     });
 }
-
-/*
-    console.log('setUpVideoList');
-    let videoListDivs = $('#videos').children();
-    videoListDivs.each((index) => jQuery(videoListDivs[index]).click(videoClickHandler));
-function videoClickHandler(el) {
-    if($(this).find('.watch-invite').length > 0) {
-        $(this).find('.watch-invite').toggle();
-    else{
-         jQuery(el.target).after(watchInviteDiv);
-         */
