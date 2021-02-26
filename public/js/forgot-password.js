242b40f027d8acbd1ae0ba37593204a2ac2151c8
Dorian Maldonado <d1maldon@ucsd.edu>
2021-02-25 18:16:50 -0800

    Update link

--- public/js/forgot-password.js
+++ public/js/forgot-password.js
@@ -13,7 +13,7 @@ function setUpSubmitButton() {
     $('#submit').click(() => {
         //use if viewing from local node server
         window.alert("Check your email to reset your password.");
-        window.location.href = "http://localhost:3000/login"; 
+        window.location.href = "https://team7-a6.herokuapp.com/login"; 
 
         //use if viewing from remote heroku server
         // window.location.href = "herokulink.com/login"
