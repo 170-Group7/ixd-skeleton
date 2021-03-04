0bc17d24306ec32a2abc6fcb1d0b4b0dc14f7878
Pamela Ghag <pghag@ucsd.edu>
2021-03-04 15:06:39 -0800

    added callback for FB

--- public/js/login.js
+++ public/js/login.js
@@ -36,7 +36,9 @@ function setUpLogInButton() {
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
- 
+FB.getLoginStatus(function(response) {
+     statusChangeCallback(response);
+ });
  
 function checkLoginState() {
    FB.getLoginStatus(function(response) {
