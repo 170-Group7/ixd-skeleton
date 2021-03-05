40e62311ceae385545dfc39b9db7e09079c7fae1
HermesKeng <kaining1101@gmail.com>
2021-03-04 22:35:44 -0800

    try fblogin

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,57 @@
-'use strict';
-import {auth} from './firebase-init.js';
-
+"use strict";
+import { auth } from "./firebase-init.js";
+let isFB = false;
 $(document).ready(function () {
-    initializePage();
+  initializePage();
 });
 
 function initializePage() {
-    auth.onAuthStateChanged((user) => {
-        if(user) {
-            let atIndex = user.email.indexOf('@');
-            let name = user.email.slice(0, atIndex);
-            fillName(name);
-        }
-        else {
-            window.location.href = '/login';
-        }
-    })
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      let atIndex = user.email.indexOf("@");
+      let name = user.email.slice(0, atIndex);
+      fillName(name);
+    } else {
+      checkLoginState();
+      //if (!isFB) window.location.href = "/login";
+    }
+  });
 }
 
 function fillName(name) {
-    let text = $('#greeting').text();
-    $('#greeting').text(`${text} ${name}`);
+  let text = $("#greeting").text();
+  $("#greeting").text(`${text} ${name}`);
+}
+
+async function checkLoginState() {
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+}
+
+function statusChangeCallback(response) {
+  console.log("Facebook login status changed.");
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === "connected") {
+    // Logged into your app and Facebook.
+    console.log("Successfully logged in with Facebook");
+    isFB = true;
+    FB.api("/me?fields=name,first_name,picture.width(480)", changeUser);
+  }
+  if (response.status === "unknown") {
+    // Logged into your app and Facebook.
+    alert("Facebook login failed. Please refresh the page and try again.");
+  }
+}
+
+function changeUser(response) {
+  $(".facebookLogin").hide();
+  window.location.href = "/index";
+  fillName(name);
+  //$("#name").text(response.name);
+  //$("#photo").attr("src", response.picture.data.url);
 }
--- public/js/login.js
+++ public/js/login.js
@@ -1,47 +1,44 @@
-'use strict';
-import {auth} from './firebase-init.js';
+"use strict";
+import { auth } from "./firebase-init.js";
 
 $(document).ready(function () {
-    initializePage();
-} );
+  initializePage();
+});
 
 function initializePage() {
-    auth.onAuthStateChanged((user) => {
-        if(user) {
-            window.location.href = '/index';
-        }
-        else {
-            setUpLogInButton();
-        }
-    })
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      window.location.href = "/index";
+    } else {
+      setUpLogInButton();
+    }
+  });
 }
 
 function setUpLogInButton() {
-    $('#log').click(() => {
-        //get user name / email
-        const userName = $('#Uname').val();
-        const pass = $('#Pass').val();
+  $("#log").click(() => {
+    //get user name / email
+    const userName = $("#Uname").val();
+    const pass = $("#Pass").val();
 
-        auth.signInWithEmailAndPassword(userName, pass)
-            .then((userCredential) => {
-                //use if viewing from local node server
-                window.location.href = '/index';
+    auth
+      .signInWithEmailAndPassword(userName, pass)
+      .then(userCredential => {
+        //use if viewing from local node server
+        window.location.href = "/index";
 
-                //use if viewing from remote heroku server
-                // window.location.href = 'https://team7-a6.herokuapp.com/index'
-            })
-            .catch((e) => alert(e));
-    });
+        //use if viewing from remote heroku server
+        // window.location.href = 'https://team7-a6.herokuapp.com/index'
+      })
+      .catch(e => alert(e));
+  });
 }
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });
-FB.getLoginStatus(function(response) {
-     statusChangeCallback(response);
- });
- 
+$(".message a").click(function () {
+  $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
+});
+
 function checkLoginState() {
-   FB.getLoginStatus(function(response) {
-     statusChangeCallback(response);
-   });
- }
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+}
